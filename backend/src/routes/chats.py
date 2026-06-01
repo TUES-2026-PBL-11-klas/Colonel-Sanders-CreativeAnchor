9afc7fdfb8c92554c1f58d2fc077919078634fe2
krasavecotklasa <cegrig4@gmail.com>
2026-06-01 21:56:46 +0300

    add: chat & image deletion

--- backend/src/routes/chats.py
+++ backend/src/routes/chats.py
@@ -1,3 +1,12 @@
 from flask_smorest import Blueprint
+from src.schemas.ChatSchema import NewChatSchema
+from flask import jsonify
 
 blp = Blueprint("Chats", "chats", description="Chat endpoints.")
+
+
+@blp.route("/chat", methods=["POST"])
+@blp.arguments(NewChatSchema)
+def newChat(json_data):
+    image = json_data["image_uuid"]
+    return jsonify({"image": image}), 201
