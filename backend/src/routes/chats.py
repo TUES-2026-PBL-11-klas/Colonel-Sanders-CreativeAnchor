a5b77d64a9eed7de4b650e6ee444e110d9d9003a
krasavecotklasa <cegrig4@gmail.com>
2026-06-03 18:20:18 +0300

    add: ai(gemini) basic functionality

--- backend/src/routes/chats.py
+++ backend/src/routes/chats.py
@@ -1,6 +1,8 @@
 from flask_smorest import Blueprint
 from src.schemas.ChatSchema import NewChatSchema
 from flask import jsonify
+from src.services.supabase import _Client
+from src.services.GeminiService import _chat2, _chat
 
 blp = Blueprint("Chats", "chats", description="Chat endpoints.")
 
@@ -8,5 +10,16 @@ blp = Blueprint("Chats", "chats", description="Chat endpoints.")
 @blp.route("/chat", methods=["POST"])
 @blp.arguments(NewChatSchema)
 def newChat(json_data):
-    image = json_data["image_uuid"]
-    return jsonify({"image": image}), 201
+
+    image_uuid = json_data["image_uuid"]
+    image_url = _Client.storage.from_("chat_images").get_public_url(str(image_uuid))
+    image_data = _Client.storage.from_("chat_images").download(str(image_uuid))
+
+    # res = _chat2(image_url)
+    res = _chat(image_data)
+    print(res)
+    # return jsonify({"image": res}), 201
+    # return res, 200
+    return jsonify({
+        "text": res.candidates[0].content.parts[0].text
+    }), 201
